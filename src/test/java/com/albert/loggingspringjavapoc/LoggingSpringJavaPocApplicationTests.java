6f40b82728973b11e9f669728d0e2ad10f7633b7
albert <amyik100@gmail.com>
2023-05-11 15:39:06 +0900

    init

---
+++ src/test/java/com/albert/loggingspringjavapoc/LoggingSpringJavaPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albert.loggingspringjavapoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingSpringJavaPocApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
